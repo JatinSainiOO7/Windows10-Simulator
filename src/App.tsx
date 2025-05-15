15ea2563f35491e97e71db11317dcc7fa9a4b2b2
JatinSainiOO7 <jatinsiani.developer@gmail.com>
2025-05-15 13:00:58 +0530

    intial commit setup project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <center>
+      <h1>
+        Welcome to Windows 10 Simulator...
+      </h1>
+    </center>
+    </>
+  )
+}
+
+export default App
